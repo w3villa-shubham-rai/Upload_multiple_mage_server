52b91b09bba7740e0345d18bcfc877e8d68659d0
w3villa-shubham-rai <shubham.rai@w3villa.com>
2023-11-05 23:24:44 +0530

    all image select from gallery

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'dart:ffi';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/src/extension_navigation.dart';
-import 'package:get/state_manager.dart';
 import 'package:image_picker/image_picker.dart';
 
 void main() {
@@ -38,10 +37,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-   final  homecontroller =  Get.put(Homecontroller());
+  final homecontroller = Get.put(Homecontroller());
   @override
   Widget build(BuildContext context) {
-   
     var arrcolors = [
       Colors.red,
       Colors.green,
@@ -68,35 +66,45 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        homecontroller.selectimage();
+                      },
                       child: Text("Pick Image"),
                     ),
                     SizedBox(width: 16.0),
                     ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        homecontroller.uploadImage();
+                      },
                       child: Text("Upload Image"),
                     ),
                   ],
                 ),
               ),
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 8, right: 8, left: 8),
-                child: GridView.builder(
-                  itemBuilder: (context, index) {
-                    return Container(
-                      color: arrcolors[index],
-                    );
-                  },
-                  itemCount: arrcolors.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 8,
-                      mainAxisSpacing: 8),
+            Obx(
+              () => Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, right: 8, left: 8),
+                  child: GridView.builder(
+                    itemBuilder: (context, index) {
+                      return Image.file(
+                        File(homecontroller.listimagespath[index]),
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      );
+                    },
+                    itemCount: homecontroller.selectedFileCount.value,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            crossAxisSpacing: 8,
+                            mainAxisSpacing: 8),
+                  ),
                 ),
               ),
-            ),
+            )
           ],
         ));
   }
@@ -135,5 +143,29 @@ class Homecontroller extends GetxController {
     selectedFileCount.value = listimagespath.length;
   }
 
-  void imageupload() {}
+  Future<void> uploadImage() async {
+  //   setState(() {
+  //     showSpiner = true;
+  //   });
+  //   var headers = {
+  //     'Content-Type': 'application/json',
+  //     'Authorization':
+  //         'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoyLCJ0aW1lIjoxNjk4OTAzMjAxfQ.WNljZRXXQ9RnGDnYCU4jSqoiGWh7db_PtAVfgo0LBCA'
+  //   };
+  //   var request = http.MultipartRequest(
+  //       "PUT", Uri.parse('https://staging.simmpli.com/api/v1/profiles/2.json'));
+
+  //   request.files.add(
+  //       await http.MultipartFile.fromPath('profile[profile_pic]', image!.path));
+  //   request.headers.addAll(headers);
+  //   var response = await request.send().timeout(const Duration(seconds: 30));
+
+  //   print("responsce :${response.statusCode}");
+  //   if (response.statusCode == 201 || response.statusCode == 200) {
+  //     print("responsce :$response ");
+  //     setState(() {
+  //        showSpiner = true;
+  //     });
+  //   }
+  }
 }
